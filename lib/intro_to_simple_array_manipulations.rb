792799b5d135000e7160dd7e2bf8ec3e6d526ab0
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 22:32:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,6 +5,8 @@ def using_push(colors_in_rainbow, next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  
   bouroughs_in_nyc.unshift(new_neighborhood)
 end 
 
