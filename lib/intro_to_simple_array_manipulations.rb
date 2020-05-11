30ee58d306084327f5203a11057336853f92d05d
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 22:02:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end 
 
 def using_pop(bouroughs_in_nyc)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.pop
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"
+  continents.pop
 end 
   
