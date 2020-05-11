e09f3471921d4ba96aa6ce1879d7035d5d6a992d
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 22:00:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(colors_in_rainbow, next_color)
+  colors_in_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", ]
+  next_color = "violet"
+  colors_in_rainbow.push(next_color)
+end
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end 
+
+def using_pop()
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = bouroughs_in_nyc.pop
+  
+end 
+  
